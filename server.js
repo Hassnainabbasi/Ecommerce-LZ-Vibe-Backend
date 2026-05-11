8bf0164830270f10f88cf23c77c80e5f474d81c7
Hassnainabbasi <168479142+Hassnainabbasi@users.noreply.github.com>
2026-05-11 18:12:34 +0500

    Add

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ const app = express();
 // app.use(cors());
 app.use(
   cors({
-    origin: "https://ecommerce-lz-vibe.vercel.app/", // No wildcard, exact port
+    origin: "https://ecommerce-lz-vibe-backend.vercel.app/", // No wildcard, exact port
     credentials: true, // Required since you're using cookies/auth
   }),
 );
